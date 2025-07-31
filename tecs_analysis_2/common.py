d65e349424526743ea65b89b1fbf2fd0ca1a87cb
George Zogopoulos <tailwhipx4@gmail.com>
2025-07-31 19:17:54 +0200

    Updated results

--- tecs_analysis_2/common.py
+++ tecs_analysis_2/common.py
@@ -12,11 +12,11 @@ LOGS_PATH = str(Path(SITL_RUN_DIR) / "logs")
 ARTIFACTS_PATH = str(Path(PROJECT_DIR) / "artifacts")
 
 parameter_matrix = {
-    "TECS_HDEM_TCONST": np.linspace(0.5, 5, 5),
-    "TECS_HGT_OMEGA": np.linspace(1, 10, 5),
-    "TECS_INTEG_GAIN": np.linspace(0.1, 1, 5),
-    "TECS_PTCH_DAMP": np.linspace(0.1, 1.0, 5),
-    "TECS_SPD_OMEGA": np.linspace(0.5, 5, 5),
-    "TECS_TIME_CONST": np.linspace(1, 10, 5),
-    "TECS_VERT_ACC": np.linspace(3, 10, 5),
+    "TECS_HDEM_TCONST": np.linspace(0.5, 5, 5),  # Default: 3
+    "TECS_HGT_OMEGA": np.linspace(1, 10, 5),  # Default: 3
+    "TECS_INTEG_GAIN": np.linspace(0.1, 1, 5),  # Default: 0.3
+    "TECS_PTCH_DAMP": np.linspace(0.1, 1.0, 5),  # Default: 0.3
+    "TECS_SPD_OMEGA": np.linspace(0.5, 5, 5),  # Default: 2
+    "TECS_TIME_CONST": np.linspace(1, 10, 5),  # Default: 5
+    "TECS_VERT_ACC": np.linspace(3, 50, 5),  # Default: 7
 }
